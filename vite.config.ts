e9005c837c6bb60f9a1bf0e19fe965038fef7428
vino-volare <vinovolare@gmail.com>
2022-09-01 13:44:12 +0900

    configurated

--- vite.config.ts
+++ vite.config.ts
@@ -94,8 +94,8 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
       manifest: {
-        name: 'Vitesse',
-        short_name: 'Vitesse',
+        name: 'TokinoVino',
+        short_name: 'TokinoVino',
         theme_color: '#ffffff',
         icons: [
           {
