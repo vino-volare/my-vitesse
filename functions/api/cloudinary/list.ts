087134f4638550da88826626105418783b017bc8
vino-volare <vinovolare@gmail.com>
2022-09-03 18:17:05 +0900

    functionsとcloudinary search api の実装

---
+++ functions/api/cloudinary/list.ts
@@ -0,0 +1,65 @@
+interface body {
+  total_count: number
+  time: number
+  aggregations: object
+  next_cursor: string
+  resources: Array<resource>
+}
+interface resource {
+  asset_id: string
+  public_id: string
+  folder: string
+  filename: string
+  format: string
+  version: number
+  resource_type: string
+  type: string
+  created_at: string
+  uploaded_at: string
+  bytes: number
+  backup_bytes: number
+  width: number
+  height: number
+  aspect_ratio: number
+  pixels: number
+  access_control: Array<object>
+  context: object
+  image_analysis: object
+  url: string
+  secure_url: string
+  status: string
+  access_mode: string
+}
+interface info {
+  filename: string
+  secure_url: string
+}
+const postBody = {
+  expression: 'folder=website/static',
+  max_results: 30,
+}
+const getImage = async (key, secret, cloudName) => {
+  const init = {
+    body: JSON.stringify(postBody),
+    method: 'POST',
+    headers: {
+      'Authorization': `Basic ${btoa(`${key}:${secret}`)}`,
+      'content-type': 'application/json;charset=UTF-8',
+    },
+  }
+  const url = `https://api.cloudinary.com/v1_1/${cloudName}/resources/search`
+  const resp = await fetch(url, init)
+  const body: body = await resp.json()
+  const resource = body.resources
+  const info: info[] = resource.map((data: resource) => {
+    const extract: info = { filename: data.filename, secure_url: data.secure_url }
+    return extract
+  })
+  return JSON.stringify(info)
+}
+
+export async function onRequest({ env }) {
+  // Contents of context object
+  const info = await getImage(env.CLOUDINARY_API_KEY, env.CLOUDINARY_API_SECRET, env.CLOUDINARY_CLOUD_NAME)
+  return new Response(info)
+}
--- src/pages/makeMd.vue
+++ src/pages/makeMd.vue
@@ -9,17 +9,90 @@ const saveMd = () => {
     saveAs(blob, `${filename.value}.md`)
   }
 }
+const mdEditor = ref(null)
+const index = ref(0)
+const insert = (element: string) => {
+  if (mdEditor.value !== null) {
+    index.value = mdEditor.value.selectionStart
+    let correctText: string
+    const escape = /\N|\n| |\u00A0|\t/
+    if (escape.test(string.value[index.value - 1]) && escape.test(string.value[index.value]))
+      correctText = `${element}`
+    else if (escape.test(string.value[index.value - 1]) || (index.value === 0))
+      correctText = `${element}\u00A0`
+    else
+      correctText = `\u00A0${element}\u00A0`
+    string.value = `${string.value.substring(0, index.value)}${correctText}${string.value.substring(index.value)}`
+  }
+}
+const isImage = ref(false)
+const toggleImage = () => {
+  isImage.value = !isImage.value
+}
+const putImage = (alt: string, url: string) => {
+  const text = `![${alt}](${url})`
+  insert(text)
+  toggleImage()
+}
+interface info {
+  secure_url: string
+  filename: string
+}
+const jsonBody: info[] = reactive([])
+const getImage = async () => {
+  const url = '/api/cloudinary/list'
+  const res = await fetch(url)
+  const data: info[] = await res.json()
+  data.map(obj => jsonBody.push(obj))
+}
+onMounted(getImage)
 </script>
 
 <template>
-  <div>
+  <div class="wrapper">
     <input v-model="filename" type="text">
-    <textarea v-model="string" />
+    <textarea ref="mdEditor" v-model="string" />
     <button btn m-3 text-sm @click="saveMd">
       save
     </button>
+    <button btn m-3 text-sm @click="toggleImage">
+      insert
+    </button>
+    <ul v-if="isImage" class="imageList">
+      <li v-for="(detail, i) in jsonBody" :key="i" class="imageWrapper">
+        <img :src="detail.secure_url" :alt="detail.filename" class="image" @click="putImage(detail.filename, detail.secure_url)">
+      </li>
+    </ul>
   </div>
 </template>
+
+<style scoped>
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+  }
+  .imageList {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .imageWrapper {
+    display: block;
+    position: relative;
+    overflow: hidden;
+    margin: 10px 5px;
+    width: 20vw;
+    height: 30vw;
+  }
+  .image {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+</style>
 
 <route lang="yaml">
 meta:
